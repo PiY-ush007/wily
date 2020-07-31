f0884352703a63b07f3b94115adac3724d100fa4
PiY-ush007 <60534991+PiY-ush007@users.noreply.github.com>
2020-07-31 18:03:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SearchScreen from './screens/SearchScreen';
+import BTScreen from './screens/BookTransactionScreen';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+
+export default class App extends React.Component{
+  render(){
+    return(
+
+    <AppContainer/>
+    )
+  }
+}
+const TabNavigator=createBottomTabNavigator({
+  Transaction:{screen:BTScreen},
+  Search:{screen:SearchScreen}
+
+})
+const AppContainer=createAppContainer(TabNavigator)
---
+++ SearchScreen.js
@@ -0,0 +1,12 @@
+import * as React from 'React';
+import {View, TouchableOpacity,Image,TextInput} from 'react-native';
+
+export default class SearchScreen extends React.Component{
+    render(){
+        return(
+            <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                <Text>Search</Text>
+            </View>
+        )
+    }
+}
